0be7e1bbdd911a0d556feecff50042a608f38a43
errinjohnson <errin.johnson@gmail.com>
2015-05-23 12:14:00 -0400

    creating central repo for projects

---
+++ AvenuePlaza/README.md
@@ -0,0 +1,22 @@
+# AvenuePlaza
+Computer Basics Course | Avenue Plaza Residence!
+
+See Wiki Pages for Collorbrating and Brainstorming: https://github.com/errinjohnson/AvenuePlaza/wiki
+
+Note: This project will also be best served with the foundation
+of Best Practices and Responsive Design, including but not limited
+to maintaining code and updates.  
+
+To create a user friendly modules that can aid in teaching
+residence the basics of computer software, hardware and internet.
+Stepping stone to Cloud Computing.  Internet based instructions, 
+so residence can practice at home with thier PC or Notebook with purpose and focus;
+meet up once a week for questions and support.  
+
+I would like to create a foundation basic computer course that other communities
+can extrapulate from and make their own, based on their needs and skill set.  
+Flexible and Intuitive Course based on communities needs.  The purpose is to
+pass this program on to other residence within the community, who became empowered
+thru the course and wishes to particate and contribute, creating a mindset of 
+coherhence and cooperation vs. the old ill informed survial of the fittest mentality.
+
---
+++ AvenuePlaza/computerbasics.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<?php
+     $pageTitle = "Course Modules";
+    include("inc/head.php"); ?>
+<body class="animsition">
+    <?php include("inc/header.php"); ?>
+      <div>
+        <div id="reveal">
+            <!-- Triggers the modals -->
+            <a href="#" data-reveal-id="vidModal" class="radius button medium-6 columns">Computer Basics Video&hellip;</a>
+            <a href="#" data-reveal-id="vid2Modal" class="radius button medium-6 columns">Operating Systems Video&hellip;</a>
+            <a href="#" data-reveal-id="courseModal" class="radius button medium-6 columns">Placement Holder&hellip;</a>
+            <a href="#" data-reveal-id="listPrint" class="radius button medium-6 columns">Placement Holder&hellip;</a>
+
+        </div>
+    </div>
+    <div>
+         <!-- Begin Video Modal -->
+    <div id="vidModal" class="reveal-modal" data-reveal>
+        <h3>What is a computer?</h3>
+        <div class="flex-video widescreen">
+            <iframe width="640" height="360" src="https://www.youtube.com/embed/7cXEOWAStq4?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <a class="close-reveal-modal">&#215;</a>
+    </div>
+     <div id="vid2Modal" class="reveal-modal" data-reveal>
+        <h3>Operating Systems</h3>
+        <div class="flex-video widescreen">
+            <iframe width="640" height="360" src="https://www.youtube.com/embed/pTdSs8kQqSA?feature=player_embedded" frameborder="0" allowfullscreen></iframe>
+        </div>
+        <a class="close-reveal-modal">&#215;</a>
+    </div>
+    <!-- Reveal Modals end -->
+    </div>
+<?php
+    include("inc/footer.php");
+    include("inc/script.php");
+?>
+</body>
+</html>
+
