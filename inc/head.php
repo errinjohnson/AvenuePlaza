7ea456d9fe836d7b9f0670c8e73635132bdf5eb5
errinjohnson <errin.johnson@gmail.com>
2015-05-25 08:04:22 -0400

    refactoring  website

--- inc/head.php
+++ inc/head.php
@@ -1,12 +1,15 @@
- <head>
+<head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>
-        <?php echo $pageTitle; ?> 
-     </title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
-    <script src="js/vendor/modernizr.js"></script>
-    <link rel="stylesheet" href="animsition-master/dist/css/animsition.min.css">
+     <title>
+        <?php echo $pageTitle; ?>
+    </title>
+    <?php date_default_timezone_set( 'EST');?>
+    <link rel="stylesheet" href="../foundation/css/foundation.css" />
+    <link rel="stylesheet" href="../foundation-icons/foundation-icons.css" />
+    <script src="../js/vendor/modernizr.js"></script>
+    <link rel="stylesheet" href="../animsition-master/dist/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="../slick-1.5.0/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="../slick-1.5.0/slick/slick-theme.css" />
     <link rel="stylesheet" href="myCss/application.css" />
 </head>
