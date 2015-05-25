eb382405aeac36692b4dfdd199d062c960636e93
errinjohnson <errin.johnson@gmail.com>
2015-05-25 07:58:37 -0400

    refactoring website

--- inc/head.php
+++ inc/head.php
@@ -4,9 +4,12 @@
     <title>
         <?php echo $pageTitle; ?> 
      </title>
-    <link rel="stylesheet" href="css/foundation.css" />
-    <link rel="stylesheet" href="foundation-icons/foundation-icons.css" />
+    <link rel="stylesheet" href="../animsition-master/dist/css/animsition.min.css">
+    <link rel="stylesheet" type="text/css" href="../slick-1.5.0/slick/slick.css" />
+    <link rel="stylesheet" type="text/css" href="../slick-1.5.0/slick/slick-theme.css" />
+    <link rel="stylesheet" href="../css/foundation.css" />
+    <link rel="stylesheet" href="../foundation-icons/foundation-icons.css" />
     <script src="js/vendor/modernizr.js"></script>
-    <link rel="stylesheet" href="animsition-master/dist/css/animsition.min.css">
+    <link rel="stylesheet" href="../animsition-master/dist/css/animsition.min.css">
     <link rel="stylesheet" href="myCss/application.css" />
 </head>
